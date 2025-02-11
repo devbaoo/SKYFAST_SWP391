e7a93b7ee8273aed3802cf5030b7f5399b760c4e
Alain <vuthanhduc181203@gmail.com>
2025-02-11 22:41:23 +0700

    hello

---
+++ src/main/java/com/example/skyfast_2_0/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.example.skyfast_2_0.dto;
+
+import com.example.skyfast_2_0.constant.Role;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+
+public class UserDTO {
+    private Integer id;
+    private String userName;
+    private String email;
+    private String password;
+    private String fullName;
+    private Integer phoneNumber;
+    private String address;
+    private Role role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updateAt;
+}
