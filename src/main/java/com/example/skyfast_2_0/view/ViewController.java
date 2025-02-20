780b82a7f9a112c2688d6be76d64a754a3f3f8b0
DylanVu <vuthanhduc181203@gmail.com>
2025-02-20 10:26:00 +0700

    Big Update v7

---
+++ src/main/java/com/example/skyfast_2_0/view/ViewController.java
@@ -0,0 +1,30 @@
+package com.example.skyfast_2_0.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// View Controller
+@Controller
+public class ViewController {
+
+
+    @GetMapping("/users/management")
+    public String userManagement(Model model) {
+        model.addAttribute("currentPage", "userlist");
+        return "userlist";
+    }
+
+    @GetMapping("/tickets/management")
+    public String ticketManagement(Model model) {
+        model.addAttribute("currentPage", "ticketManagement");
+        return "ticketManagement";
+    }
+
+    @GetMapping("/airline/management")
+    public String airlineManagement(Model model) {
+        model.addAttribute("currentPage", "airlineManagement");
+        return "airlineManagement";
+    }
+}
