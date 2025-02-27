51d8617e5fc794c93d67244eb6e663f33a4b3be2
DylanVu <vuthanhduc181203@gmail.com>
2025-02-27 13:40:41 +0700

    Big Update v16

---
+++ src/main/java/com/example/skyfast_2_0/repository/PromotionRepository.java
@@ -0,0 +1,7 @@
+package com.example.skyfast_2_0.repository;
+
+import com.example.skyfast_2_0.entity.Promotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromotionRepository extends JpaRepository<Promotion, Integer> {
+}
